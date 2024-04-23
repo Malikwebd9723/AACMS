93f47203f991fdf8736d5f91da3b074180feb794
malikwebd9723 <malik9723usman@gmail.com>
2024-04-24 00:44:17 +0500

    add signup page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
       <>
       <Routes>
       <Route exact path="/" element={<Login />} />
+      <Route exact path="/signup" element={<SignUp />} />
       </Routes>
       </>
   );
