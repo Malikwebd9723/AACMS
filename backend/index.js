f24715deb2d7cd89f4dd0ff905dcfd3f0b4c5565
malikwebd9723 <malik9723usman@gmail.com>
2024-05-16 22:39:23 +0500

    connect to db

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+const port = 8002;
+const cors = require("cors");
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose
+    .connect(`mongodb+srv://developer9723usman:iZMcMcKbjEydMekw@cluster0.lemgu3v.mongodb.net/`)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.log("Error connecting to MongoDb", err.message);
+    });
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
