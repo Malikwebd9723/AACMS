a55a9d6ee5371a9c060093721c16e3dd9bb87ee9
Zainshah55 <syedzain5454@gmail.com>
2024-05-29 07:07:31 -0700

    commit

--- backend/models/cases.js
+++ backend/models/cases.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 const { type } = require("@testing-library/user-event/dist/type");
->>>>>>> 2868027d7ebda3f6a4b6cd845d1e4cd3e099208c
 const mongoose = require("mongoose");
 
 const casesSchema = new mongoose.Schema({
@@ -9,13 +6,10 @@ const casesSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-<<<<<<< HEAD
-=======
     lawyerId: {
         type: String,
         required: true,
     },
->>>>>>> 2868027d7ebda3f6a4b6cd845d1e4cd3e099208c
     caseTitle: {
         type: String,
         required: true,
@@ -28,23 +22,7 @@ const casesSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-<<<<<<< HEAD
-    totalCases: {
-        type: String,
-        required: true,
-    },
-    filedDate: {
-        type: String,
-        required: true,
-    },
-    judgeName: {
-        type: String,
-        required: true,
-    },
-    courtName: {
-=======
     judge: {
->>>>>>> 2868027d7ebda3f6a4b6cd845d1e4cd3e099208c
         type: String,
         required: true,
     },
@@ -57,22 +35,6 @@ const casesSchema = new mongoose.Schema({
         required: true,
     },
     totalFee: {
-<<<<<<< HEAD
-        type: String,
-        required: true,
-    },
-    discount: {
-        type: String,
-        required: true,
-    },
-    paidFee: {
-        type: String,
-        required: true,
-    },
-    paidFeeDate: {
-        type: String,
-        required: true,
-=======
         type: Number,
         deafault:0
     },
@@ -87,7 +49,6 @@ const casesSchema = new mongoose.Schema({
     paidFeeDate: {
         type: Date,
         default:Date.now
->>>>>>> 2868027d7ebda3f6a4b6cd845d1e4cd3e099208c
     },
     hearingDate: {
         type: String,
@@ -97,52 +58,6 @@ const casesSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-<<<<<<< HEAD
-    //   password: {
-    //     type: String,
-    //     default: "",
-    //   },
-    //   dutyPlace: {
-    //     type: String,
-    //     default: "",
-    //   },
-    //   lastattendance: {
-    //     type: Array,
-    //     default:[{status:"",date:""}]
-    //   },
-    //   attendance: {
-    //     type: Array,
-    //     default:[{status:"",date:""}]
-    //   },
-    //   checkIn:{
-    //     type:Array,
-    //     default:[{date:"",latitude:"",longitude:""}]
-    //   },
-    //   checkOut:{
-    //     type:Array,
-    //     default:[{date:"",latitude:"",longitude:""}]
-    //   },
-    //   emailVerified: {
-    //     type: Boolean,
-    //     default: false,
-    //   },
-    //   userVerified: {
-    //     type: Boolean,
-    //     default: false,
-    //   },
-    //   verificationToken: {
-    //     type: String,
-    //     default: "",
-    //   },
-
-    //    orders: [
-    //        {
-    //          type: mongoose.Schema.Types.ObjectId,
-    //          ref: "Order",
-    //        },
-    //    ],
-=======
->>>>>>> 2868027d7ebda3f6a4b6cd845d1e4cd3e099208c
     createdAt: {
         type: Date,
         default: Date.now,
@@ -151,8 +66,4 @@ const casesSchema = new mongoose.Schema({
 
 const Cases = mongoose.model("Cases", casesSchema);
 
-<<<<<<< HEAD
 module.exports = Cases;
-=======
-module.exports = Cases;
->>>>>>> 2868027d7ebda3f6a4b6cd845d1e4cd3e099208c
